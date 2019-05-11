9f2ddca110a1b55df6a0b1d140a6dedf310be9b1
Dydier Ramirez <dydier.ramirez@gmail.com>
2019-05-11 12:50:44 -0500

    Cargue inicial BoticApp

---
+++ BoticApp/Controllers/UsuarioController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BoticApp.Models;
+
+namespace BoticApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private SLAutenticacionEntities1 db = new SLAutenticacionEntities1();
+
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View(db.USUARIO.ToList());
+        }
+
+        // GET: Usuario/Details/5
+        public ActionResult Details(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            if (uSUARIO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIO);
+        }
+
+        // GET: Usuario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuario/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_USUARIO,NOMBRES,APELLIDOS,EMAIL,USUARIO1,CLAVE,ESFARMACIA,RAZON_SOCIAL,NIT,DIRECCION,TELEFONO,FECHA_CREACION,FECHA_MODIFICACION")] USUARIO uSUARIO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USUARIO.Add(uSUARIO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(uSUARIO);
+        }
+
+        // GET: Usuario/Edit/5
+        public ActionResult Edit(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            if (uSUARIO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIO);
+        }
+
+        // POST: Usuario/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_USUARIO,NOMBRES,APELLIDOS,EMAIL,USUARIO1,CLAVE,ESFARMACIA,RAZON_SOCIAL,NIT,DIRECCION,TELEFONO,FECHA_CREACION,FECHA_MODIFICACION")] USUARIO uSUARIO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSUARIO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(uSUARIO);
+        }
+
+        // GET: Usuario/Delete/5
+        public ActionResult Delete(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            if (uSUARIO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIO);
+        }
+
+        // POST: Usuario/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(decimal id)
+        {
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            db.USUARIO.Remove(uSUARIO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
